4200844f1c6ad6a37175df047dd8bc2cad586314
Faduma Abdillahi <faduma92@gmail.com>
2020-10-27 17:57:08 -0400

    Updates

---
+++ precache-manifest.89d8a37cbab20e279d4a46a90ba6c52c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a733de2fba48653be6cd2924172d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18d62adbba97e00b3fcb",
+    "url": "/static/css/main.97cc4f10.chunk.css"
+  },
+  {
+    "revision": "b4d38c79f6852bfd47bf",
+    "url": "/static/js/2.93e59f8e.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/static/js/2.93e59f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d62adbba97e00b3fcb",
+    "url": "/static/js/main.42550353.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
